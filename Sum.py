78443625ab193c7a17eb66b4cedb57425ef724c2
abeselom-tsegazeab <105537591+abeselom-tsegazeab@users.noreply.github.com>
2023-01-21 15:00:13 +0300

    Update Sum.py

--- Sum.py
+++ Sum.py
@@ -1,1 +1,11 @@
 
+x = int(input())
+ans = []
+for i in range(x):
+    sets = input().split()
+    if int(sets[2]) == int(sets[0]) + int(sets[1]) or int(sets[1]) == int(sets[0]) + int(sets[2]) or int(sets[0]) == int(sets[1]) + int(sets[2]):
+        ans.append('yes')
+    else:
+        ans.append('no')
+for i in ans:
+    print(i)
